ec7bba8e4dcd8f6f19b1995b9a5e84941097aeed
Micheal Parks <michealparks1989@gmail.com>
2016-11-15 17:44:22 -0500

    bugger fixers

--- app/main/autolaunch.js
+++ app/main/autolaunch.js
@@ -2,15 +2,12 @@ const AutoLaunch = require('auto-launch')
 const { app, ipcMain } = require('electron')
 const { sendToMenubar } = require('./ipc')
 
-// On Mac, work around a bug in auto-launch where it opens a Terminal window
-// See https://github.com/Teamwork/node-auto-launch/issues/28#issuecomment-222194437
-const appPath = process.platform === 'darwin'
-  ? app.getPath('exe').replace(/\.app\/Content.*/, '.app')
-  : undefined // Use the default
-
 const launcher = new AutoLaunch({
   name: 'Galeri',
-  path: appPath
+  // On Mac, work around a bug in auto-launch where it opens a Terminal window
+  // See https://github.com/Teamwork/node-auto-launch/issues/28#issuecomment-222194437
+  path: app.getPath('exe').split('.app/Content')[0] + '.app',
+  isHidden: false
 })
 
 launcher.isEnabled().then(function (isEnabled) {
