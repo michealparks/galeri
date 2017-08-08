57e9188a78ed1aa768384ab06954b7fa194cad80
Micheal Parks <michealparks1989@gmail.com>
2017-08-08 12:08:43 -0400

    update

--- app/background/museums-new/api-keys.js
+++ app/background/museums-new/api-keys.js
@@ -1,5 +1,4 @@
 module.exports = {
-  cooperHewitt: '065eb1fb4238bb6af1fbf030b2ccade8',
   harvard: 'bd7001a0-ae80-11e6-a622-c1c937457c9a',
   rijks: 'xPauC1vP',
   walters: 'ar9kcanGaRe3Wk4b5wyNdcqZJgYS7VQoQihXukTZPGwtHrxG78hfCZJx1aQv1K95'
