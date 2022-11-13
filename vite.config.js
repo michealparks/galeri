d28886bd1d1363179307f8ea34ddc16e528bed22
Micheal Parks <michealparks1989@gmail.com>
2022-11-13 00:00:21 -0500

    add blend files

--- vite.config.js
+++ vite.config.js
@@ -15,10 +15,15 @@ export default defineConfig({
   envPrefix: ["VITE_", "TAURI_"],
   build: {
     // Tauri supports es2021
-    target: ["es2021", "chrome100", "safari13"],
+    target: ["es2021"],
     // don't minify for debug builds
     minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
     // produce sourcemaps for debug builds
     sourcemap: !!process.env.TAURI_DEBUG,
+    rollupOptions: {
+      input: {
+        about: 'about.html',
+      },
+    },
   },
 });
